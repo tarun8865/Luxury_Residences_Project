8bced97a6f36d142506814889aed5d651cdd3ac4
Tarun Kumar <tarunkumar08965@gmail.com>
2025-08-14 00:09:58 +0530

    Deploy

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ app.use('/api/listing', listingRouter);
 app.use(express.static(path.join(_dirname, '/client/dist')));
 
 app.get('*', (req, res)=>{
-    res.senFile(path.join(_dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
